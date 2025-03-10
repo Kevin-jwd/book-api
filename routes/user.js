66ef6a74b7d1dae2d32085f09ee8a5a069980b8f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-10 23:20:57 +0900

    feat: user.js 라우트 추가
    - 회원가입 : POST /register
    - 로그인 : POST /auth/login
    - 비밀번호 초기화 요청 : POST /auth/password-reset
    - 비밀번호 초기화 : PUT /auth/password-reset

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+// express module
+const express = require("express");
+const router = express.Router();
+
+// 회원가입
+router.post("/register", (req, res) => {
+    res.json({});
+});
+
+// 로그인
+router.post("/auth/login", (req, res) => {
+    res.json({});
+});
+
+// 비밀번호
+router
+.route("/auth/password-reset")
+    // 초기화 요청
+    .post("/auth/password-reset", (req, res) => res.json({}))
+    // 초기화
+    .put("/auth/password-reset", (req, res) => res.json({}));
