331f253b0b095e68d8139729923754ebca9dfc98
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-11 20:16:59 +0900

    feat(userController): 로그인 라우터 작성 및 비밀번호 관련 라우트 모듈화

--- controller/userController.js
+++ controller/userController.js
@@ -1,22 +1,80 @@
 // mariadb module
 const conn = require("../mariadb");
 
+// jwt module
+const jwt = require("jsonwebtoken");
+
+// dotenv module
+const dotenv = require("dotenv");
+dotenv.config();
+
+// http-status-codes module
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+
+// 회원가입 모듈
 const register = (req, res) => {
     const { email, name, password, contact } = req.body;
     const sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`;
     const values = [email, name, password, contact];
 
-    conn.query(sql, values, (err, result) => {
+    conn.query(sql, values, (err, results) => {
         if (err) {
-            return res.status(StatusCodes.BAD_REQUEST).json({
-                message: ReasonPhrases.BAD_REQUEST,
-            });
+            return res.status(StatusCodes.BAD_REQUEST).end();
         }
         return res.status(StatusCodes.CREATED).json({
             message: ReasonPhrases.CREATED,
-            value: values,
+            value: results,
         });
     });
 };
 
-module.exports = register;
+// 로그인 모듈
+const login = (req, res) => {
+    const { email, password } = req.body;
+    const sql = `SELECT * FROM users WHERE users.email = ?`;
+
+    conn.query(sql, email, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        const loginUser = results[0];
+        if (!loginUser || loginUser.password !== password) {
+            return res.status(StatusCodes.UNAUTHORIZED).end();
+        }
+
+        const token = jwt.sign(
+            {
+                email: loginUser.email,
+            },
+            process.env.PRIVATE_KEY,
+            {
+                expiresIn: "30m",
+                issuer: "Do",
+            }
+        );
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 1000 * 60 * 60 * 24,
+        });
+
+        return res.status(StatusCodes.OK).json({
+            message: ReasonPhrases.OK,
+            results: results,
+        });
+    });
+};
+
+// 비밀번호 초기화 요청 모듈
+const passwordResetRequest = (req, res) => {};
+
+// 비밀번호 초기화 모듈
+const passwordResetConfirm = (req, res) => {};
+
+module.exports = {
+    register,
+    login,
+    passwordResetRequest,
+    passwordResetConfirm,
+};
