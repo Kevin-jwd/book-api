ed9a441591f45ca95e682c3d417a6d7f68ddf22f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-14 17:31:47 +0900

    feat: 도서 전체 조회 라우트 작성

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,15 +1,22 @@
 // mariadb module
 const conn = require("../mariadb");
 
-// dotenv module
-const dotenv = require("dotenv");
-dotenv.config();
-
 // http-status-codes module
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
-// 전체 도서 조회 모듈듈
-const getAllBooks = (req, res) => {};
+// 전체 도서 조회 모듈
+const getAllBooks = (req, res) => {
+    const sql = `SELECT * FROM books`;
+    conn.query(sql, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        return res.status(StatusCodes.OK).json({
+            message: ReasonPhrases.OK,
+            results: results,
+        });
+    });
+};
 
 // 개별 도서 상세 정보 조회 모듈
 const getDetailedBook = (req, res) => {};
