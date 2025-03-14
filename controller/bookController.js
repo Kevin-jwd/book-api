4e7df7a7c8b865afd3ba4143897995294bf09789
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-14 22:49:21 +0900

    feat: 도서 개별 조회 라우트 작성

--- controller/bookController.js
+++ controller/bookController.js
@@ -19,7 +19,24 @@ const getAllBooks = (req, res) => {
 };
 
 // 개별 도서 상세 정보 조회 모듈
-const getDetailedBook = (req, res) => {};
+const getDetailedBook = (req, res) => {
+    const { id } = req.params;
+    const value = [id];
+
+    const sql = `SELECT * FROM books WHERE id = ?`;
+    conn.query(sql, value, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        if (!results[0]) {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+        return res.status(StatusCodes.OK).json({
+            message: ReasonPhrases.OK,
+            results: results,
+        });
+    });
+};
 
 // 장르별 도서 조회 모듈
 const getBooksByGenres = (req, res) => {};
