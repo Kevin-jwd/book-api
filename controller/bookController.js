65a9d6739aa332191ba8c0d5625d4877f61876e2
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-16 18:25:54 +0900

    feat: 장르별 도서 조회 라우트 작성
    - 전체 도서 조회 API에서 쿼리 파라미터의 유무에 따라 전제 도서 조회와 장르별 도서 조회로 분기하도록 구현

--- controller/bookController.js
+++ controller/bookController.js
@@ -4,16 +4,27 @@ const conn = require("../mariadb");
 // http-status-codes module
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
-// 전체 도서 조회 모듈
-const getAllBooks = (req, res) => {
-    const sql = `SELECT * FROM books`;
-    conn.query(sql, (err, results) => {
+// 전체 도서 및 장르별 도서서 조회 모듈
+const getBooks = (req, res) => {
+    const { genre_id } = req.query;
+    let sql = `SELECT * FROM books`;
+    const values = [];
+
+    if (genre_id) {
+        sql += ` WHERE genre_id = ?`;
+        values.push(genre_id);
+    }
+
+    conn.query(sql, values, (err, results) => {
         if (err) {
             return res.status(StatusCodes.BAD_REQUEST).end();
         }
+        if (!results.length) {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
         return res.status(StatusCodes.OK).json({
             message: ReasonPhrases.OK,
-            results: results,
+            results,
         });
     });
 };
@@ -38,11 +49,7 @@ const getDetailedBook = (req, res) => {
     });
 };
 
-// 장르별 도서 조회 모듈
-const getBooksByGenres = (req, res) => {};
-
 module.exports = {
-    getAllBooks,
+    getBooks,
     getDetailedBook,
-    getBooksByGenres,
 };
--- routes/book.js
+++ routes/book.js
@@ -2,19 +2,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    getAllBooks,
-    getDetailedBook,
-    getBooksByGenres,
-} = require("../controller/bookController");
+const { getBooks, getDetailedBook } = require("../controller/bookController");
 
-// 전체 도서 조회
-router.get("/", getAllBooks);
+// 전체 도서 및 장르별 도서 조회
+router.get("/", getBooks);
 
 // 개별 도서 조회
 router.get("/:id", getDetailedBook);
-
-// 장르별 도서 조회
-router.get("/", getBooksByGenres);
 
 module.exports = router;
