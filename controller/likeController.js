ecc6a4f5faba6feec31d941c6a765547caaae4ba
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-19 15:22:00 +0900

    feat: 좋아요 추가 기본 틀 작성

--- controller/likeController.js
+++ controller/likeController.js
@@ -2,8 +2,24 @@ const conn = require("../mariadb");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const router = require("../routes/user");
 
+// 좋아요 추가
 const addLike = (req, res) => {
-    res.json("좋아요 추가");
+    const { user_id } = req.body;
+    const { book_id } = req.params;
+
+    const value = [user_id, book_id];
+
+    const sql = `INSERT INTO likes (user_id, book_id) VALUES (?, ?)`;
+    conn.query(sql, value, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        return res.status(StatusCodes.CREATED).json({
+            message: ReasonPhrases.CREATED,
+            results: results,
+        });
+    });
 };
 
 const removeLike = (req, res) => {
