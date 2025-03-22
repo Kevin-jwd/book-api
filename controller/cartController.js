090d73e0b97600bc8f188864708d87d4521d397a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-22 18:07:32 +0900

    feat: 장바구니 담기 API 구현

--- controller/cartController.js
+++ controller/cartController.js
@@ -5,8 +5,22 @@ const conn = require("../mariadb");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
 const addCartItems = (req, res) => {
-    res.json("장바구니 담기");
+    const { user_id, book_id, quantity } = req.body;
+
+    const value = [user_id, book_id, quantity];
+
+    const sql = `INSERT INTO cart_items (user_id, quantity, book_id) VALUES (?, ?, ?)`;
+    conn.query(sql, value, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        return res.status(StatusCodes.OK).json({
+            message: ReasonPhrases.OK,
+            results: results,
+        });
+    });
 };
+
 const getCartItems = (req, res) => {
     res.json("장바구니 조회");
 };
